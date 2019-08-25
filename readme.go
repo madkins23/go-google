74d9f6beb9b8fa96abe768910fab0c0d2ea8733f
mAdkins <software@doorways.org>
2019-08-25 13:02:23 -0700

    License provided in separate file, no need to document

--- readme.go
+++ readme.go
@@ -4,10 +4,4 @@
 // they're simplistic mechanisms implemented to solve specific problems.
 // Projects defined in other mAdkins23 modules use this code.
 // If you need this functionality consider copying the code into your own project.
-//
-// License
-//
-// This package is provided via the MIT license.
-// Do what you will shall be the whole of the law.
-// No warranty expressed or implied.
 package go_common
