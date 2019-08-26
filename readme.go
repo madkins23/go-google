3bb1b540420cd82e6f9476e79f60c5c68a41dee9
mAdkins <software@doorways.org>
2019-08-25 18:24:00 -0700

    Renaming of project to remove hyphen.

--- readme.go
+++ readme.go
@@ -1,7 +1,7 @@
-// Common Go packages shared by various mAdkins23 projects.
+// Google API utilities.
 //
 // These are not really production quality, they're simplistic mechanisms implemented to solve specific problems.
 // This code is Linux-dependent due to assumptions made about the SSH directory location.
 //
 // If you need this functionality consider copying the code into your own project and modifying accordingly.
-package go_common
+package google
